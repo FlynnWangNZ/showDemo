6188a423ebdeb1df65ee82f8e26f114427f09763
FlynnWangNZ <fei.wang@lincolnuni.ac.nz>
2023-01-02 14:50:03 +1300

    add function send mail

--- setup_email/views.py
+++ setup_email/views.py
@@ -1,14 +1,14 @@
 import logging
 
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.http import JsonResponse, QueryDict
 from django.shortcuts import render
-
-
 # Create your views here.
 from django.views import View
 
 from setup_email.utils import get_svn_version
-from showDemo.settings import COMPONENTS
+from showDemo.settings import COMPONENTS, EMAIL_HOST_USER
 
 
 class SetupEmailView(View):
@@ -24,6 +24,8 @@ class SetupEmailView(View):
         logging.debug(f'SetupEmailView post {parameters_dict}')
         title = parameters_dict.get('emailTitle').rstrip()
         send_to = parameters_dict.get('sendTo').split()
+        user = User.objects.get(username=requests.user)
+        send_to.append(user.email)
 
         # 2. fill up the html email template and send it out
         """There should be an html email template. I just use text to make it simple"""
@@ -32,7 +34,7 @@ class SetupEmailView(View):
             if key in ['sendTo', 'emailTitle', 'csrfmiddlewaretoken']:
                 continue
             email_content += f'{key}: {value} \n\n'
-
+        send_mail(title, email_content, EMAIL_HOST_USER, send_to, fail_silently=False)
         # 3. write data to database
         return render(requests, 'setup_email/index.html', {
             'components': COMPONENTS
