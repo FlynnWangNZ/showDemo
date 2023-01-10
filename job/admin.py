05d780be526b259bbdad2818576ca7c8b1a44b49
FlynnWangNZ <fei.wang@lincolnuni.ac.nz>
2023-01-10 17:01:34 +1300

    add models and admin

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
-
 # Register your models here.
+from django.utils.timezone import now
+
+from job.models import SourceSiteModel, JobApplicationRecordModel, ApplicationDetailModel, AppointmentModel
+
+
+class SourceSiteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address']
+    list_display_links = list_display
+
+
+class JobApplicationRecordAdmin(admin.ModelAdmin):
+    list_display = ['apply_date', 'job_title', 'company_name', 'hr_email', 'source_site', 'job_detail_id']
+    list_display_links = list_display
+    readonly_fields = ['added_by']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.filter(added_by=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.added_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+class ApplicationDetailAdmin(admin.ModelAdmin):
+    list_display = ['job_detail_id', 'direction', 'summary', 'email_keyword']
+    list_display_links = list_display
+    readonly_fields = ['added_by']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.filter(added_by=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.added_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+
+    def coming_next_week(self, obj):
+        return obj.is_active and (obj.appointment_time - now()).days < 7
+
+    list_display = ['company_name', 'appointment_time', 'is_active', 'feed_back', 'coming_next_week']
+    list_display_links = list_display
+    readonly_fields = ['added_by']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.filter(added_by=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.added_by = request.user
+        obj.save()
+
+
+admin.site.register(SourceSiteModel, SourceSiteAdmin)
+admin.site.register(JobApplicationRecordModel, JobApplicationRecordAdmin)
+admin.site.register(ApplicationDetailModel, ApplicationDetailAdmin)
+admin.site.register(AppointmentModel, AppointmentAdmin)
--- job/models.py
+++ job/models.py
@@ -1,3 +1,75 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# Job Application Record
+
+# Site Table
+# ID, Name, URL
+class SourceSiteModel(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Job Source Site')
+    address = models.URLField(verbose_name='Site Address')
+
+    class Meta:
+        verbose_name_plural = verbose_name = 'Source Site'
+
+    def __str__(self):
+        return self.name
+
+
+# Record Table
+# ID, Date, JobTitle, JobDescription, CompanyName, HREmail, SourceSite, CV, CoverLetter, Details
+class JobApplicationRecordModel(models.Model):
+    apply_date = models.DateField(verbose_name='Application Date')
+    job_title = models.CharField(max_length=100, verbose_name='Job Title')
+    job_description = models.TextField(verbose_name='Job Description')
+    company_name = models.CharField(max_length=100, verbose_name='Company Name', unique=True, null=False)
+    hr_email = models.EmailField(verbose_name='HR Email')
+    source_site = models.ForeignKey(SourceSiteModel, on_delete=models.CASCADE, verbose_name='Source Site')
+    cv = models.FileField(verbose_name='CV')
+    cover_letter = models.FileField(verbose_name='Cover Letter')
+    job_detail_id = models.IntegerField(verbose_name='Detail ID', unique=True, null=False)
+    added_by = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = verbose_name_plural = 'Job Application Record'
+
+    def __str__(self):
+        return f'{self.job_title} at {self.company_name}'
+
+
+# Detail Table
+# ID, JobDetailID(ForeignKey), Direction(send/receive), Summary, EmailKeyword
+class ApplicationDetailModel(models.Model):
+    job_detail_id = models.ForeignKey(JobApplicationRecordModel, to_field='job_detail_id', on_delete=models.CASCADE)
+    direction = models.IntegerField(verbose_name='Direction', choices=(
+        (0, 'send'),
+        (1, 'receive')
+    ))
+    summary = models.CharField(max_length=200, verbose_name='Summary')
+    email_keyword = models.CharField(max_length=100, verbose_name='Email Keyword',
+                                     help_text='key word to search in the email inbox')
+    added_by = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = verbose_name = 'Application Detail'
+
+    def __str__(self):
+        return self.summary
+
+
+# Appointments
+# ID, CompanyName, AppointmentTime, IsActive, Feedback, ComingNextWeek
+class AppointmentModel(models.Model):
+    company_name = models.ForeignKey(JobApplicationRecordModel, to_field='company_name', verbose_name='Company Name',
+                                     on_delete=models.CASCADE)
+    appointment_time = models.DateTimeField(verbose_name='Appointment Time')
+    is_active = models.BooleanField(verbose_name='Is Active', help_text='False if it has been done')
+    feed_back = models.TextField(verbose_name='Feedback')
+    added_by = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = verbose_name = 'Appointments'
+
+    def __str__(self):
+        return f'{self.appointment_time}'
