1db617f4d95e43b47fbd4957a788174f21be8070
FlynnWangNZ <fei.wang@lincolnuni.ac.nz>
2023-02-20 21:58:42 +1300

    build backend database models

--- grading/admin.py
+++ grading/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from grading.models import CourseModel, AssignmentModel
+
+
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'semester', 'start_date', 'end_date']
+    list_display_links = list_display
+
+
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'marks', 'percentage', 'course']
+    list_display_links = list_display
+    list_filter = ['course']
+
+
+admin.site.register(CourseModel, CourseAdmin)
+admin.site.register(AssignmentModel, AssignmentAdmin)
--- grading/models.py
+++ grading/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CourseModel(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Course Name')
+    semester = models.CharField(max_length=50, verbose_name='Semester')
+    start_date = models.DateField(verbose_name='Start Date', null=True, blank=True)
+    end_date = models.DateField(verbose_name='End Date', null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = verbose_name = 'Course'
+
+    def __str__(self):
+        return self.name
+
+
+class AssignmentModel(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Assignment Name')
+    marks = models.IntegerField(verbose_name='Marks in Total')
+    percentage = models.IntegerField(verbose_name='Percentage')
+    course = models.ForeignKey(CourseModel, on_delete=models.CASCADE, verbose_name='Course Name')
+
+    class Meta:
+        verbose_name_plural = verbose_name = 'assignment'
+
+    def __str__(self):
+        return self.name
